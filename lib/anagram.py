f657914b541b6dbd71e43c84f24cb73d411658ff
Ryan Rizeq <ryanrizeq@gmail.com>
2023-07-20 18:22:53 -0700

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, list):
+        matches = []
+        for word in list:
+            if len(self.word) == len(word):
+                if sorted(self.word.lower()) == sorted(word.lower()):
+                    matches.append(word)
+        return matches
